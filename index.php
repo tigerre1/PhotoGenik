aa9dd36fe836708b3f315304e147c1b2e0828160
Daniel Cortes <renaissance.scholar2012@gmail.com>
2016-09-26 00:51:36 +0000

    Began transforming HTML and CSS into CMS PHP

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+  	<div class="w3-row">
+	  <div class="w3-col m4 14 pic">
+	   <a href="single.html">
+	    <img src="/images/1.jpg" class="w3-animate-opacity">
+           </a>
+	   <p>Etsy chillwave mlkshk drinking vinegar, keytar seitan unicorn. Williamsburg meh viral drinking vinegar hoodie. Church-key kogi neutra, etsy truffaut vegan VHS mlkshk la croix health goth synth squid paleo.</p>
+	  </div>
+	  <div class="w3-col m4 14 pic">
+	    <img src="/images/2.jpg" class="w3-animate-opacity">
+	    <p>Etsy chillwave mlkshk drinking vinegar, keytar seitan unicorn. Williamsburg meh viral drinking vinegar hoodie. Church-key kogi neutra, etsy truffaut vegan VHS mlkshk la croix health goth synth squid paleo.</p>
+	  </div>
+	  <div class="w3-col m4 14 pic" class="w3-animate-opacity"> 
+ 	   <img src="/images/3.jpg">
+	   <p>Etsy chillwave mlkshk drinking vinegar, keytar seitan unicorn. Williamsburg meh viral drinking vinegar hoodie. Church-key kogi neutra, etsy truffaut vegan VHS mlkshk la croix health goth synth squid paleo.</p>	   
+ 	  </div>
+	  <div class="w3-col m4 14 pic" class="w3-animate-opacity">
+	   <img src="/images/4.jpg">
+	   <p>Etsy chillwave mlkshk drinking vinegar, keytar seitan unicorn. Williamsburg meh viral drinking vinegar hoodie. Church-key kogi neutra, etsy truffaut vegan VHS mlkshk la croix health goth synth squid paleo.</p>
+	  </div>
+          <div class="w3-col m4 14 pic" class="w3-animate-opacity">
+           <img src="/images/5.jpg">
+	   <p>Etsy chillwave mlkshk drinking vinegar, keytar seitan unicorn. Williamsburg meh viral drinking vinegar hoodie. Church-key kogi neutra, etsy truffaut vegan VHS mlkshk la croix health goth synth squid paleo.</p>
+ 	  </div>
+	  <div class="w3-col m4 14 pic" class="w3-animate-opacity"> 
+	   <img src="/images/6.jpg">
+ 	   <p>Etsy chillwave mlkshk drinking vinegar, keytar seitan unicorn. Williamsburg meh viral drinking vinegar hoodie. Church-key kogi neutra, etsy truffaut vegan VHS mlkshk la croix health goth synth squid paleo.</p>
+	  </div>
+
+<?php get_footer(); ?>
